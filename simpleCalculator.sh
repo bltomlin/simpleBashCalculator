eca0a56989ebbd06310a5c1b6ed6bca23b3b0db5
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2023-05-08 19:41:17 +0000

    more functionality, update readme

--- simpleCalculator.sh
+++ simpleCalculator.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env bash
 
-echo "Enter an arithmetic operation:"
-read input
-re='^[-0-9\.]+ [-,+,*,/,^] [-0-9\.]+$'
-if [[ $input =~ $re ]]; then
-    arithmetic_result=$(echo "scale=2; $input" | bc -l)
-    printf "%s\n" "$arithmetic_result"
-else
-    echo "Operation check failed!"
-fi
+touch operation_history.txt
+
+echo "Welcome to the basic calculator!" | tee -a operation_history.txt
+echo "Enter an arithmetic operation or type 'quit' to quit:" | tee -a operation_history.txt
+read -r input
+echo "$input" >> operation_history.txt
+
+while [ "$input" != "quit" ]
+do
+
+    re='^[-0-9\.]+ [-,+,*,/,^] [-0-9\.]+$'
+    if [[ $input =~ $re ]]; then
+        arithmetic_result=$(echo "scale=2; $input" | bc -l)
+        printf "%s\n" "$arithmetic_result" | tee -a operation_history.txt
+    else
+        echo "Operation check failed!" | tee -a operation_history.txt
+    fi
+    echo "Enter an arithmetic operation or type 'quit' to quit:" | tee -a operation_history.txt
+    read -r input
+    echo "$input" >> operation_history.txt
+
+done
+echo "Goodbye!" | tee -a operation_history.txt
